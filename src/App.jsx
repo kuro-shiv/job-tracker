7ce832eb38c45e73b2bd6599e1c43823be4dc93a
kuro-shiv <dubeysk2003@gmail.com>
2025-12-19 23:24:39 +0530

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App() {
+  return ( 
+    <div className='min-h-screen bg-slate-100'>
+      <Navbar />
+      <main className='max-w-7xl mx-auto p-4'>
+        <h2 className='text-xl font-semibold'>Welcome to the Job Tracker App</h2>
+        <p className='mt-2 text-gray-700'>Track your job applications efficiently.</p>
+      </main> 
+    </div>
   )
 }
-
-export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="w-w-full bg-slate-900 text-white shadow-md"> 
+            <div className="max-w-7xl mx-auto px-4">
+                <h1 className="text-2xl font-bold py-4">
+                    Job Tracker
+                </h1>
+            <span className="absolute right-8 top-6 cursor-pointer md:hidden">
+                React+ tailwind
+            </span>
+            </div>
+
+        </nav>
+    )}
