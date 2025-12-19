12558106649da4c3ee6f4579dc9b615ed00ec2f0
kuro-shiv <dubeysk2003@gmail.com>
2025-12-19 23:31:09 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return ( 
     <div className='min-h-screen bg-slate-100'>
       <Navbar />
       <main className='max-w-7xl mx-auto p-4'>
-        <h2 className='text-xl font-semibold'>Welcome to the Job Tracker App</h2>
-        <p className='mt-2 text-gray-700'>Track your job applications efficiently.</p>
+        <h2 className='text-xl font-semibold'>
+          Welcome to the Job Tracker App
+        </h2>
+        <p className='mt-2 text-gray-600'>
+          Track your job applications efficiently.
+        </p>
       </main> 
     </div>
   )
